7a9562d741b109a1cb867a167a2ee232a38e09c7
Natalia Zharkova <zharkonat@mail.ru>
2021-06-16 10:33:50 +0300

    adding the socials

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,16 +3,16 @@ export const Footer = () => {
 	return (<>
 		<div className="socials">
 			<ul className="footer">
-				<a href="#">
+				<a href="https://www.instagram.com/" target="_blank">
 					<li><img src="./images/socials/Instagram.png" alt="Instagram" /></li>
 				</a>
-				<a href="#">
+				<a href="https://www.facebook.com/" target="_blank">
 					<li><img src="./images/socials/Facebook.png" alt="Facebook" /></li>
 				</a>
-				<a href="#">
+				<a href="https://twitter.com/" target="_blank">
 					<li><img src="./images/socials/Twitter.png" alt="Twitter" /></li>
 				</a>
-				<a href="#">
+				<a href="https://www.whatsapp.com/" target="_blank">
 					<li><img src="./images/socials/WhatsApp.png" alt="WhatsApp" /></li>
 				</a>
 			</ul>
